0c5e732cf7c781b08d215d1fa07b840bbc5f91a3
Sergio Belmar <sbelmar@gmail.com>
2026-02-22 13:32:02 -0300

    backup

---
+++ demos/WUIModal-basic/main.js
@@ -0,0 +1,24 @@
+const init = () => {
+	const button = document.getElementById("my-button");
+	const modal = new WUIModal({
+		selector: ".wui-modal.my-modal",
+		//openDelay: 200,
+		//onStartOpen: null,
+		onOpen: () => {
+			console.log("Modal opened");
+		},
+		//onMaximize: null,
+		//onScrolling: null,
+		//onStartClose: null,
+		onClose: () => {
+			console.log("Modal closed");
+		}
+		//onBack: null
+	});
+	modal.init();
+	button.addEventListener("click", () => {
+		modal.open();
+	});
+}
+
+window.addEventListener("DOMContentLoaded", init);
